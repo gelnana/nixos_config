e277ca23f69a563e9d8bf09022bab76dd01c5705
gelnana <chloe@semios.is>
2025-08-18 13:12:58 -0700

    mpv

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -12,22 +12,22 @@
       ytdl-format = "bestvideo+bestaudio/best";
     };
 
-
     scripts = with pkgs.mpvScripts; [
-        mpris
-        quality-menu
-        uosc
-        sponsorblock
+      mpris
+      quality-menu
+      uosc
+      sponsorblock
     ];
-        package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override {
+
+    package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override {
       vulkanSupport = true;
     }) {
       youtubeSupport = true;
     };
 
-  home.packages = with pkgs; [
-    yt-dlp   # needed for streaming from YouTube and others
-    aria2    # for segmented downloading
-  ];
+    home.packages = with pkgs; [   # ❌ wrong place
+      yt-dlp
+      aria2
+    ];
   };
 }
