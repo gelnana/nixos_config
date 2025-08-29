7146cefcc57fbe1877d4fd62ffa1cbecb8b086ec
gelnana <chloe@semios.is>
2025-08-29 13:21:14 -0700

    added hypr

---
+++ home/desktop/hypr/default.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hyprland-config.nix
+  ];
+  }
--- users/gelnana/home.nix
+++ users/gelnana/home.nix
@@ -3,9 +3,11 @@
 {
   imports = [
     ../../home/core.nix
-
     ../../home/programs
     ../../home/shell
+
+    ../../home/desktop/plasma
+    ../../home/desktop/hypr
   ];
     # Enable programs
   programs.git = {
