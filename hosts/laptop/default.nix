6be4bd1cbc3b247de15bf0c104dbd88d9e96ee47
gelnana <chloe@semios.is>
2025-09-09 07:38:30 -0700

    modified:   hosts/laptop/default.nix

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -83,6 +83,8 @@
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = true;
 
+    powerManagement.memwap.enable = true;
+
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of
