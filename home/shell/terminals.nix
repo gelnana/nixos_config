5b778572c0c11efee93a6356aeccab867f636948
gelnana <chloe@semios.is>
2025-09-20 17:29:40 -0700

    plymouth

--- home/shell/terminals.nix
+++ home/shell/terminals.nix
@@ -9,7 +9,7 @@
       settings = {
         window_padding_width = 5;
         copy_on_select = "clipboard";
-        font_family = "SF Pro";
+        font_family = "SF Pro, JuliaMono";
         bold_font = "SF Pro Bold";
         italic_font = "SF Pro Italic";
         bold_italic_font = "SF Pro Bold Italic";
