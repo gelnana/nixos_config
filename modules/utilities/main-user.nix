ac8b0b8c0340e6a03b6da4bda8db70fa345eb8f3
gelnana <chloe@semios.is>
2025-10-03 17:20:01 -0700

    modified:   modules/utilities/main-user.nix

--- modules/utilities/main-user.nix
+++ modules/utilities/main-user.nix
@@ -1,78 +1,80 @@
 { lib, config, pkgs, username ? "gelnana", ... }:
-let
-  cfg = config.main-user;
-in {
-  options.main-user = {
-    enable = lib.mkEnableOption "main user module";
 
-    userName = lib.mkOption {
-      type = lib.types.str;
-      default = username;
-      description = "Primary username";
-    };
+{
+  options = {
+    main-user = {
+      enable = lib.mkEnableOption "main‑user module";
 
-    shell = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.nushell;
-      description = "Default shell for the user";
-    };
+      userName = lib.mkOption {
+        type = lib.types.str;
+        default = username;
+        description = "Primary username";
+      };
 
-    hashedPasswordFile = lib.mkOption {
-      type = lib.types.nullOr lib.types.path;
-      default = "/persist/etc/shadow/${username}";
-      description = "Path to file containing hashed password";
-    };
+      shell = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.nushell;
+        description = "Default shell for the user";
+      };
 
-    initialPassword = lib.mkOption {
-      type = lib.types.str;
-      default = "changeme";
-      description = "Initial password (fallback if hashedPasswordFile doesn't exist)";
-    };
+      hashedPasswordFile = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = "/persist/etc/shadow/${username}";
+        description = "Path to file containing hashed password";
+      };
 
-    extraGroups = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [
-        "wheel"
-        "networkmanager"
-        "video"
-        "audio"
-        "input"
-      ];
-      description = "Additional groups for the user";
-    };
+      initialPassword = lib.mkOption {
+        type = lib.types.str;
+        default = "changeme";
+        description = "Initial password (fallback if hashedPasswordFile doesn't exist)";
+      };
 
-    enableSudo = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Enable sudo for wheel group";
-    };
+      extraGroups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ "wheel" "networkmanager" "video" "audio" "input" ];
+        description = "Additional groups for the user";
+      };
 
-    mutableUsers = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Whether to allow mutable users";
+      enableSudo = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable sudo for wheel group";
+      };
+
+      mutableUsers = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = "Whether to allow mutable users";
+      };
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    users.mutableUsers = cfg.mutableUsers;
+  config = lib.mkIf config.main-user.enable (let
+    cfg = config.main-user;
+  in
+    {
+      users.mutableUsers = cfg.mutableUsers;
 
-    users = {
-      root = {
-        initialPassword = cfg.initialPassword;
-        hashedPasswordFile = "/persist/etc/shadow/root";
-      };
-      ${cfg.userName} = {
-        isNormalUser = true;
-        description = cfg.userName;
-        shell = cfg.shell;
-        extraGroups = cfg.extraGroups;
-        initialPassword = cfg.initialPassword;
-        hashedPasswordFile = cfg.hashedPasswordFile;
+      users.users = {
+        root = {
+          isNormalUser = false;
+          initialPassword = cfg.initialPassword;
+          hashedPasswordFile = "/persist/etc/shadow/root";
         };
 
+        ${cfg.userName} = {
+          isNormalUser = true;
+          createHome = true;
+          home = "/home/${cfg.userName}";
+          description = "User ${cfg.userName}";
+          shell = cfg.shell;
+          extraGroups = cfg.extraGroups;
+          initialPassword = cfg.initialPassword;
+          hashedPasswordFile = cfg.hashedPasswordFile;
+        };
+      };
+
       security.sudo.enable = cfg.enableSudo;
-      environment.shells = [ cfg.shell ];
-    };
-  };
+    }
+  );
 }
