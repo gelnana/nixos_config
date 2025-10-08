fa32fb9a98be6fdba1dc383921f7e4fb938616a9
gelnana <chloe@semios.is>
2025-10-07 17:29:40 -0700

    modified:   modules/hardware/mounts.nix

--- modules/hardware/mounts.nix
+++ modules/hardware/mounts.nix
@@ -7,11 +7,9 @@
   cfg = config.custom.zfs.storage;
 in {
   options.custom.zfs.storage = {
-    enable = lib.mkEnableOption "Additional ZFS storage pools";
-
-    data = lib.mkEnableOption "Data pool";
-
-    archive = lib.mkEnableOption "Archive pool";
+    enable = lib.mkEnableOption "ZFS storage pools";
+    data = lib.mkEnableOption "data pool";
+    archive = lib.mkEnableOption "archives pool";
   };
 
   config = lib.mkIf cfg.enable {
@@ -24,12 +22,44 @@ in {
         "/data/active" = {
           device = "data/active";
           fsType = "zfs";
-          neededForBoot = false;
         };
-        "/data/projects" = {
-          device = "data/projects";
+        "/data/active/documents" = {
+          device = "data/active/documents";
           fsType = "zfs";
-          neededForBoot = false;
+        };
+        "/data/active/projects" = {
+          device = "data/active/projects";
+          fsType = "zfs";
+        };
+        "/data/active/work" = {
+          device = "data/active/work";
+          fsType = "zfs";
+        };
+
+        "/data/media" = {
+          device = "data/media";
+          fsType = "zfs";
+        };
+        "/data/media/pictures" = {
+          device = "data/media/pictures";
+          fsType = "zfs";
+        };
+        "/data/media/video" = {
+          device = "data/media/video";
+          fsType = "zfs";
+        };
+        "/data/media/games" = {
+          device = "data/media/games";
+          fsType = "zfs";
+        };
+        "/data/media/music" = {
+          device = "data/media/music";
+          fsType = "zfs";
+        };
+
+        "/data/archive" = {
+          device = "data/archive";
+          fsType = "zfs";
         };
       })
 
@@ -37,39 +67,73 @@ in {
         "/archives/media" = {
           device = "archives/media";
           fsType = "zfs";
-          neededForBoot = false;
         };
         "/archives/backups" = {
           device = "archives/backups";
           fsType = "zfs";
-          neededForBoot = false;
         };
       })
     ];
+
     services.sanoid = {
       enable = true;
-      datasets = {
-        "data/active" = {
-          autosnap = true;
-          autoprune = true;
-          hourly = 24;
-          daily = 7;
-          weekly = 4;
-          monthly = 6;
-        };
-        "archives/backups" = {
-          autosnap = true;
-          autoprune = true;
-          daily = 7;
-          weekly = 4;
-          monthly = 12;
-        };
-      };
+      datasets = lib.mkMerge [
+        (lib.mkIf cfg.data {
+          "data/active" = {
+            autosnap = true;
+            autoprune = true;
+            hourly = 24;
+            daily = 7;
+            weekly = 4;
+            monthly = 6;
+            recursive = true;
+          };
+
+          "data/media" = {
+            autosnap = true;
+            autoprune = true;
+            daily = 7;
+            weekly = 4;
+            monthly = 12;
+            recursive = true;
+          };
+
+          "data/archive" = {
+            autosnap = true;
+            autoprune = true;
+            weekly = 4;
+            monthly = 12;
+            yearly = 3;
+          };
+        })
+
+        (lib.mkIf cfg.archive {
+          "archives/backups" = {
+            autosnap = true;
+            autoprune = true;
+            daily = 7;
+            weekly = 4;
+            monthly = 12;
+          };
+        })
+      ];
     };
 
-    systemd.tmpfiles.rules = [
-      "L+ /home/${username}/Data - - - - /data/active"
-      "L+ /home/${username}/Archives - - - - /archives/media"
-    ];
+    systemd.tmpfiles.rules =
+      lib.optionals cfg.data [
+        # Active
+        "L+ /home/${username}/Documents - - - - /data/active/documents"
+        "L+ /home/${username}/Projects - - - - /data/active/projects"
+        "L+ /home/${username}/Work - - - - /data/active/work"
+
+        # Media
+        "L+ /home/${username}/Pictures - - - - /data/media/pictures"
+        "L+ /home/${username}/Video - - - - /data/media/video"
+        "L+ /home/${username}/Games - - - - /data/media/games"
+        "L+ /home/${username}/Music - - - - /data/media/music"
+      ]
+      ++ lib.optionals cfg.archive [
+        "L+ /home/${username}/Archives - - - - /archives/media"
+      ];
   };
 }
