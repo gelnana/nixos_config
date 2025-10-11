078f749a4a662f139877680ed4186d4f5990f7e1
gelnana <chloe@semios.is>
2025-10-11 10:17:50 -0700

    modified:   modules/roles/dev.nix

--- modules/roles/dev.nix
+++ modules/roles/dev.nix
@@ -4,25 +4,29 @@
   lib,
   config,
   ...
-}: {
-  config = {
-    environment.systemPackages = with pkgs;
-      [
-        git
-        gcc
-        gnumake
-        cmake
-        pkg-config
-        virt-manager
-        docker
-      ]
-      ++ [inputs.fenix.packages.${pkgs.system}.stable.toolchain];
-
-    programs.direnv.enable = true;
-
-    virtualisation.docker.enable = true;
-    virtualisation.docker.storageDriver = "zfs";
-
-    virtualisation.libvirtd.enable = true;
-  };
+}:
+let
+  cfg = config.custom.virtualisation;
+in {
+  config = lib.mkMerge [
+    {
+      environment.systemPackages = with pkgs;
+        [
+          git
+          gcc
+          gnumake
+          cmake
+          pkg-config
+          virt-manager
+          docker
+        ]
+        ++ [inputs.fenix.packages.${pkgs.system}.stable.toolchain];
+      programs.direnv.enable = true;
+    }
+    (lib.mkIf cfg.enable {
+      virtualisation.docker.enable = true;
+      virtualisation.docker.storageDriver = "zfs";
+      virtualisation.libvirtd.enable = true;
+    })
+  ];
 }
